8da6d41b275e0a8245ac83880c060768169b3354
LuxCodeFlow <im.nwong86@gmail.com>
2015-11-15 19:06:19 -0600

    Create KWage.js

---
+++ KWage.js
@@ -0,0 +1,6 @@
+var YearOfBirth = prompt("What is year were you born?");
+var userage = (2015 - YearOfBirth);
+if (userage > 25) {alert ("LIAR! Your age is 25")};
+if (userage < 25) {alert ("LIAR! Your age is 25")};
+if (userage == 25) {alert ("you're age is 25.")};
+//alert("you're " + userage + " of age");
